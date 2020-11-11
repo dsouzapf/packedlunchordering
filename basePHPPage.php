b8bf4d9cd54925d9b75de5979e517b5157fc17f3
dsouza.pf <dsouza.pf@oundleschool.org.uk>
2020-11-11 10:16:10 +0000

    Init pages

---
+++ basePHPPage.php
@@ -0,0 +1,16 @@
+<?php include_once("session_init.php"); ?>
+<!DOCTYPE html>
+<html>
+
+    <head>
+    </head>
+
+    <body>
+
+        <?php
+            include_once("connection.php");
+        ?>
+
+    </body>
+
+</html>
---
+++ basePage.php
@@ -0,0 +1,16 @@
+<?php include_once("session_init.php"); ?>
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title> - Packed Lunch Ordering</title>
+        <link rel="stylesheet" href="stylesheet.css">
+    </head>
+
+    <body>
+
+        
+
+    </body>
+
+</html>
