f26405471a576fe6ee22a407f11b057edf93038a
dsouza.pf <dsouza.pf@oundleschool.org.uk>
2020-12-12 16:57:30 +0000

    prepare change password pages

---
+++ changePassword.php
@@ -0,0 +1,18 @@
+<?php
+include_once("session_init.php");
+?>
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title> - Packed Lunch Ordering</title>
+        <link rel="stylesheet" href="stylesheet.css">
+    </head>
+
+    <body>
+
+        <button onclick="window.location.href='index.php'">Home</button>
+
+    </body>
+
+</html>
---
+++ changePasswordRun.php
@@ -0,0 +1,23 @@
+<?php
+include_once("session_init.php");
+include_once("connection.php");
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+
+<?php
+
+array_map("htmlspecialchars", $_POST);
+
+
+
+?>
+
+</body>
+
+</html>
